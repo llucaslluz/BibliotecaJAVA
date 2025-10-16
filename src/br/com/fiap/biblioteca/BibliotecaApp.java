16a680bc1287ab73cfc13667abab6d5d082b4b2c
Lucas da Luz Morais <llucaslluz@gmail.com>
2025-10-16 14:27:30 -0300

    Biblioteca

--- src/br/com/fiap/biblioteca/BibliotecaApp.java
+++ src/br/com/fiap/biblioteca/BibliotecaApp.java
@@ -1,4 +1,8 @@
 package br.com.fiap.biblioteca;
 
 public class BibliotecaApp {
+    public static void main(String[] args) {
+
+    }
+
 }
---
+++ src/br/com/fiap/biblioteca/model/Livro.java
@@ -0,0 +1,9 @@
+package br.com.fiap.biblioteca.model;
+
+public class Livro {
+    int codigo;
+    String tituto;
+    String autor;
+    String isbn;
+
+}
