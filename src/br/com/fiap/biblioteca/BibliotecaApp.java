09b2472fb56b27f20b4dd6305fa2a2206144a2d0
Lucas da Luz Morais <llucaslluz@gmail.com>
2025-10-16 14:47:46 -0300

    teste do get e set

--- src/br/com/fiap/biblioteca/BibliotecaApp.java
+++ src/br/com/fiap/biblioteca/BibliotecaApp.java
@@ -1,7 +1,16 @@
 package br.com.fiap.biblioteca;
 
+import br.com.fiap.biblioteca.model.Livro;
+
 public class BibliotecaApp {
     public static void main(String[] args) {
+
+        Livro livro = new Livro();
+        livro.setCodigo(-9);
+
+        int codigoDoLivro = livro.getCodigo();
+        System.out.println(codigoDoLivro);
+
 
     }
 
--- src/br/com/fiap/biblioteca/model/Livro.java
+++ src/br/com/fiap/biblioteca/model/Livro.java
@@ -1,9 +1,20 @@
 package br.com.fiap.biblioteca.model;
 
 public class Livro {
-    int codigo;
+    private int codigo;
     String tituto;
     String autor;
     String isbn;
 
+    public void setCodigo(int valor){
+        if (valor > 0){
+            codigo = valor;
+        } else {
+            System.out.println("VALOR INVALIDO !!!");
+        }
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
 }
